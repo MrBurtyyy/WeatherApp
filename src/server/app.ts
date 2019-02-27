f938a11e27b8d5f0141f7bb5d448159f849515d2
Alex Burt <alex@Alexs-MBP.home>
2019-02-27 00:50:59 +0000

    Add .prettierrc file for styling, and add some utility functions for loading from environment variables as well as checking if an object is undefined

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,13 @@
 import express = require("express");
-import { Utils } from "./utils";
+import { isUndefined, loadFromEnvironmentVariable } from "./utils";
 
 const app = express();
 const port = 3000;
 
-const open_weather_api_key = Utils.loadFromEnvironmentVariable(
-  "OPEN_WEATHER_API_KEY"
-);
+const open_weather_api_key = loadFromEnvironmentVariable("OPEN_WEATHER_API_KE");
+if (isUndefined(open_weather_api_key)) {
+  console.error("OpenWeather API Key is not defined");
+}
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/hello", (req, res) => res.send("This is a hello world endpoint!"));
--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,11 +1,18 @@
-export class Utils {
-  /**
-   * Returns the value contained in an environment variable
-   *
-   * @param envVar The name of the environment variable
-   * @returns The value contained in specified environment variable
-   */
-  public static loadFromEnvironmentVariable(envVar: string): any {
-    return process.env[envVar];
-  }
-}
+/**
+ * Returns the value contained in an environment variable.
+ *
+ * @param envVar The name of the environment variable
+ * @returns The value contained in specified environment variable
+ */
+export const loadFromEnvironmentVariable = function(envVar: string): any {
+  return process.env[envVar];
+};
+
+/**
+ * Checks if supplied object is undefined or not.
+ * @param objToCheck The object to check
+ * @returns True/False
+ */
+export const isUndefined = function(objToCheck: any): boolean {
+  return typeof objToCheck === "undefined";
+};
